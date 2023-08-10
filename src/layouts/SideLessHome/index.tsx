23e1d3c3b6edcbf0710cf0c5437ef8d4d626eba5
kazuki1023 <k.keio12569@gmail.com>
2023-08-10 14:39:16 +0900

    add ビデオプレイヤーのデザイン

--- src/layouts/SideLessHome/index.tsx
+++ src/layouts/SideLessHome/index.tsx
@@ -1,12 +1,22 @@
 import { Outlet } from "react-router-dom";
 import { DashboardHeader } from "../../templates/DashboardHeader";
+import useStyles from "./styles";
 
 export const SideLessHomeLayout = () => {
+  const styles = useStyles();
   return (
     <div>
+      {/*
+        DashboardHeaderコンポーネントを表示する
+      */}
       <DashboardHeader />
-      <h1>Dashboard</h1>
-      <Outlet />
+      {/*
+        <Outlet />を配置した箇所に、childrenコンポーネントが展開される
+        childrenコンポーネントとは、Route.tsx内でchildren>elementで指定したコンポーネントである
+      */}
+      <div className={styles.main}>
+        <Outlet />
+      </div>
     </div>
   );
 };
