bf37ced015e3018ac7f7cf2963859368d11f581c
kazuki1023 <k.keio12569@gmail.com>
2023-08-17 11:03:45 +0900

    add 説明欄作成

--- src/pages/Upload/index.tsx
+++ src/pages/Upload/index.tsx
@@ -1,30 +1,35 @@
-import { Dialog, DialogTitle, DialogContent, Grid, Divider} from "@material-ui/core";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import { UploadForm } from "./UploadForm";
+
+// import
 import { VideoSelect } from "./VideoSelector";
 
 export const Upload = () => {
   return (
     <Dialog fullWidth={true} maxWidth="md" open={true}>
-      {/* タイトル用コンポーネント */}
       <DialogTitle>動画のアップロード</DialogTitle>
-
-      {/* 横線コンポーネント */}
       <Divider />
-
-      {/* コンテント用コンポーネント */}
       <DialogContent>
         <Grid container spacing={4}>
           <Grid xs item>
             <VideoSelect />
           </Grid>
-
-          {/*
-            真ん中に縦線を挿入
-            デザインにあるような線を入れるため
-          */}
           <Divider orientation="vertical" flexItem />
 
+          {/*
+            左側に
+          */}
           <Grid xs item>
-            右側
+            {/*
+              UploadFormコンポーネントを表示
+            */}
+            <UploadForm />
           </Grid>
         </Grid>
       </DialogContent>
