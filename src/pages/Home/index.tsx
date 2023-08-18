40b3f56db42c7bf47db66f7e2738811783cadedc
kazuki1023 <k.keio12569@gmail.com>
2023-08-18 09:22:01 +0900

    delete fetch関数処理

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,30 @@
+// src/pages/Home/index.tsx
+
 import { Container, Grid } from "@material-ui/core";
 import { VideoCard } from "../../components/VideoCard";
 
-
-let called = false;
-
 export const Home = () => {
-  // VideoCardコンポーネントを表示する
   return (
     <Container>
+      {/* 取得したデータを表示してみる */}
       <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <VideoCard />
+        </Grid>
 
-      {/*
-        横並びにしたいコンポーネントの一つ一つを<Grid>で囲む
-        こちらのGridでは、itemプロパティを指定する。
-        全部を囲む<Grid container>の中にそれぞれの横並び要素の<Grid item>があるイメージ
-      */}
-      <Grid item xs={4}>
-        <VideoCard />
+        <Grid item xs={3}>
+          <VideoCard />
+        </Grid>
+        <Grid item xs={3}>
+          <VideoCard />
+        </Grid>
+        <Grid item xs={3}>
+          <VideoCard />
+        </Grid>
+        <Grid item xs={3}>
+          <VideoCard />
+        </Grid>
       </Grid>
-
-      <Grid item xs={4}>
-        <VideoCard />
-      </Grid>
-      <Grid item xs={4}>
-        <VideoCard />
-      </Grid>
-      <Grid item xs={4}>
-        <VideoCard />
-      </Grid>
-      <Grid item xs={4}>
-        <VideoCard />
-      </Grid>
-    </Grid>
     </Container>
   );
 };
