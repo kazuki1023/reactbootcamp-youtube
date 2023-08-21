0907bdbd8c81477b7db6fd82cf9ffd93af3c0988
kazuki1023 <k.keio12569@gmail.com>
2023-08-21 11:14:12 +0900

    動画upload処理

--- src/hooks/VideoUpload/index.ts
+++ src/hooks/VideoUpload/index.ts
@@ -3,9 +3,10 @@
 import { useEffect, useState } from "react";
 import { storage } from "../../utils/Firebase/config";
 import { v4 as uuidv4 } from "uuid";
-
-// 追加
-import { useInsertVideoMutation } from "../../utils/graphql/generated";
+import {
+  useInsertVideoMutation,
+  VideosDocument,
+} from "../../utils/graphql/generated";
 import { useRecoilValue } from "recoil";
 import { GlobalUser } from "../../stores/User";
 
@@ -16,18 +17,21 @@ type UploadProps = {
   };
   title: string;
   description?: string;
+  ownerId: string;
 };
 
 export const useVideoUpload = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error>();
 
-  // 動画のメタデータを保存するGraphQLMutation
-  const [mutation, { error: apolloError }] = useInsertVideoMutation();
+  // 追加
+  // 動画をApolloでアップロードする`mutation`に対して、キャッシュ更新を指定
+  // 今回は、`Videos`というクエリーを指定しています。
+  const [mutation, { error: apolloError }] = useInsertVideoMutation({
+    refetchQueries: [{ query: VideosDocument }],
+  });
 
-  // `video`の`ownerId`のために、userのidを取得する
   const user = useRecoilValue(GlobalUser);
-
 
   const uploadStorage = (id: string, file: File, path: string) => {
     const exe = file.name.split(".").pop();
@@ -35,16 +39,12 @@ export const useVideoUpload = () => {
   };
 
   const upload = async ({ file, title, description, ownerId }: UploadProps) => {
-    // 追加
-    // ユーザーが読み込まれていない、未ログインであれば処理を中断する
     if (!user?.id) {
       return;
     }
-
     setLoading(true);
-    const thumbId = uuidv4();
-
-    // 動画のIDを生成する
+    const videoName = uuidv4();
+    const thumbName = uuidv4();
     const videoId = uuidv4();
 
     try {
@@ -53,15 +53,12 @@ export const useVideoUpload = () => {
         file.video,
         "videos"
       );
-
       const thumbnailUploadTask = await uploadStorage(
         thumbName,
         file.thumbnail,
         "thumbnails"
       );
 
-      // 追加
-      // 動画のメタデータを保存する
       const res = await mutation({
         variables: {
           id: videoId,
@@ -72,11 +69,7 @@ export const useVideoUpload = () => {
           owner_id: ownerId,
         },
       });
-
-      // 追加
-      // 全ての処理が終わったら、動画のメタデータを返す
       return res.data?.insert_videos_one;
-
     } catch (error) {
       console.error(error);
       setError(new Error("エラーが発生しました。最初からやり直してください。"));
@@ -85,8 +78,6 @@ export const useVideoUpload = () => {
     }
   };
 
-  // 追加
-  // Apollo Clientのエラーをキャッチする
   useEffect(() => {
     if (apolloError) {
       console.error(apolloError);
@@ -100,3 +91,4 @@ export const useVideoUpload = () => {
     error,
   };
 };
+
