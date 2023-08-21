f2c7d4dfa59fd5f9f2eae66567bf7f81a309a3c5
kazuki1023 <k.keio12569@gmail.com>
2023-08-21 10:08:32 +0900

    add　関連動画表示

--- src/components/VideoHorizontalCard/index.tsx
+++ src/components/VideoHorizontalCard/index.tsx
@@ -1,43 +1,68 @@
 import { Card, CardHeader, CardMedia } from "@material-ui/core";
-import { HeaderTitle } from "../VideoCard/HeaderTitle";
-import { SubHeaderContent } from "../VideoCard/SubHeaderContent";
+import { useEffect, useState } from "react";
+import { HeaderTitle, HeaderTitleProps } from "../VideoCard/HeaderTitle";
+import {
+  SubHeaderContent,
+  SubHeaderContentProps,
+} from "../VideoCard/SubHeaderContent";
 import useStyles from "./styles";
 
-export const VideoHorizontalCard = () => {
+export type VideoHorizontalCardProps = {
+  fetcher: () => Promise<string | undefined>;
+} & HeaderTitleProps &
+  SubHeaderContentProps;
+
+// 親コンポーネントから渡されるprops
+export const VideoHorizontalCard = ({
+  title,
+  owner,
+  views,
+  created,
+  fetcher,
+}: VideoHorizontalCardProps) => {
   const styles = useStyles();
+
+  // 追加
+  // サムネイルのダウンロードリンクのステート
+  const [src, setSrc] = useState<string>();
+
+
+  // 追加
+  useEffect(() => {
+    // サムネイルのダウンロードリンクを取得する関数
+    fetcher().then(setSrc);
+  });
+
   return (
-    // elevation={0} : box-shadowの影を削除する
-    // square: border-radiusを削除する
-    //
-    // 複数のスタイルを適用したい場合、このような形で記述します。
-    // `${}`という記法を用いることで、変数の値を文字として展開できます。
-    // 例：ten = 10 → `${ten}` == "10"
-    // 詳しくはhttps://jsprimer.net/basic/data-type/#template-literal
     <Card
       className={`${styles.root} ${styles.transparent}`}
       elevation={0}
       square
     >
-      {/* 
-        サムネイル用のメディアコンポーネントを作成
-
-        サムネイルを16:9で表示するために、`CardMedia`を<div>で囲み、widthプロパティを固定しています。
-      */}
       <div className={styles.thumbnail}>
-        <CardMedia
-          className={styles.media}
-          image="/static/no-image.jpg"
-          title="Thumbnail"
-        />
+
+        {/*
+          修正
+          取得したサムネイルのダウンロードリンクを参照する
+        */}
+        <CardMedia className={styles.media} image={src} title="Thumbnail" />
       </div>
 
-      {/* 
+      {/*
         `Home`で作成した<HeaderTitle>と<SubHeaderContent>を流用する
       */}
       <CardHeader
         className={styles.contentPadding}
-        title={<HeaderTitle />}
-        subheader={<SubHeaderContent />}
+
+        // 追加
+        // タイトルを表示
+        title={<HeaderTitle title={title} />}
+
+        // 追加
+        // 投稿者名、再生回数、作成日時を表示
+        subheader={
+          <SubHeaderContent owner={owner} views={views} created={created} />
+        }
       />
     </Card>
   );
